6e96ab4cffa0fa5b878528fd369bb80d7af15e06
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-02-21 16:48:02 -0600

    require v3.6

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from distutils.core import setup
 
+import sys
+if sys.version_info[0] < (3,6):
+    sys.exit("This package requires Python v3.6 or higher.")
+
 setup(name='igv2local',
       version='0.1',
       description='IGV session remote host to local file organizer',
