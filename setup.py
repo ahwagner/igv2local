6472dba1efc219aeea39c63c99eb4732bd295f17
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-02-21 19:57:32 -0600

    create sdist

--- setup.py
+++ setup.py
@@ -3,14 +3,30 @@
 from distutils.core import setup
 
 import sys
-if sys.version_info[0] < (3,6):
+if sys.version_info < (3, 6):
     sys.exit("This package requires Python v3.6 or higher.")
 
 setup(name='igv2local',
-      version='0.1',
+      version='0.1.0a1',
       description='IGV session remote host to local file organizer',
       author='Alex Wagner',
+      url='https://github.com/ahwagner/igv2local',
       author_email='awagner24@wustl.edu',
-      py_modules=['igv2local'],
-      requires=['gmstk']
-)
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords='igv local copy',
+      packages=['igv2local'],
+      install_requires=['gmstk'],
+      entry_points={
+          'console_scripts': ['igv2local=igv2local.igv2local:main']
+      },
+      dependency_links=[
+          'https://github.com/ahwagner/gmstk/tarball/master#egg=gmstk-0.1.0a1'
+          ]
+      )
